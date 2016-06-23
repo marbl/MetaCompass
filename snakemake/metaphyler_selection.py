2d5b1380d130a7c8bd7ddd00c8ad83329044fd06
vcepeda <vickycees@gmail.com>
2016-06-23 18:33:22 -0400

    new rules

--- snakemake/metaphyler_selection.py
+++ snakemake/metaphyler_selection.py
@@ -16,13 +16,3 @@ rule reference_recruitment:
     message: """---reference recruitment."""
     shell:"mkdir -p {output.out}; ./MetaCompass-beta/bin/pickrefseqs.pl {input.fasta} {output.out} 1   1> {output.log} 2>&1"
 
-rule build_index:
-    input:
-        reffile = '{sample}.assembly.out/mc.refseq.fna'
-    output:
-       index= '{sample}.assembly.out/{sample}.index',
-       log= '{sample}.step2.log',
-       pref='{sample}.assembly.out/{sample}.index',
-       sam= '{sample}.assembly.out/{sample}.sam'
-    message: """---Build index ."""
-    shell:"bowtie2-build -q {input.reffile} {output.pref} 1>> {output.index} 2>&1;bowtie2 --end-to-end -k 30 -p 1 -x {output.pref} -f reads.fasta -S {output.sam} > {output.log} 2>&1"
