4603d59b2244bcbc62acad77bcc361917da33d15
vcepeda <vickycees@gmail.com>
2016-06-24 00:04:12 -0400

    more params

--- snakemake/mapping.py
+++ snakemake/mapping.py
@@ -1,11 +1,12 @@
 configfile: "config.json"
 rule build_index:
     input:
-        reffile = '{sample}.assembly.out/mc.refseq.fna'
+       '{sample}.assembly.out/mc.refseq.fna'
     output:
        index= '{sample}.assembly.out/{sample}.index',
-       log= '{sample}.step2.log',
        pref='{sample}.assembly.out/{sample}.index',
        sam= '{sample}.assembly.out/{sample}.sam'
+    log: '{sample}.step2.log'
+    threads:1
     message: """---Build index ."""
-    shell:"bowtie2-build -q {input.reffile} {output.pref} 1>> {output.index} 2>&1;bowtie2 --end-to-end -k 30 -p 1 -x {output.pref} -f reads.fasta -S {output.sam} > {output.log} 2>&1"
+    shell:"bowtie2-build -q {input} {output.pref} 1>> {output.index} 2>&1;bowtie2 --end-to-end -k 30 -p 1 -x {output.pref} -f reads.fasta -S {output.sam} > {log} 2>&1"
--- snakemake/pilon.py
+++ snakemake/pilon.py
@@ -1,30 +1,25 @@
 import os
+import re
 configfile: "config.json"
 rule extract_single_fasta: 
-    input:'{sample}.assembly.out/contigs.fasta'# config["references"]["genome"]#"wildcards.reference]
+    input:'{sample}.assembly.out/contigs.fasta'
     output:'{sample}.genomes/NC_000915.1_0.fasta'
     message: """---single fasta genomes.""" 
-#    shell:"echo"
-
-    run:shell("echo ")
-	references='Sample1.assembly.out/contigs.fasta'#config["references"]["genome"]
+    run:
+        references='Sample1.assembly.out/contigs.fasta'
 	f = open(references)
 	shell("mkdir -p Sample1.genomes")
 	contigs=[]
 	for line in f:
             if line[0] == ">" :
                 contigs=(line[1:].rstrip().split(' ',1)[0])	
-		shell("samtools faidx {references} {contigs}> Sample1.genomes/{contigs}.fasta")#shell("mkdir -p Sample1.g;samtools faidx {FILE} {contigs}> Sample1.g/{contigs}.fasta")
-
-#def _run_pilon(genome,bam,contig_id):
-#    shell("java -jar pilon --genome {input} --frags Sample1.pilon/Sample1.sorted.bam --output Sample1.pilon/NC_000915 --fix bases --tracks --vcf --changes  1> {output} 2>&1")
-import re
+		shell("samtools faidx {references} {contigs}> Sample1.genomes/{contigs}.fasta")
 rule correct_pilon:
-    input:'{sample}.genomes'#/NC_000915.1_0.fasta'
-    output:'{sample}.pilon/NC_000915.1_0.fasta'#{sample}.output.log'
-    run:x='Sample1'
-    rootdir = os.getcwd()+'/'+'Sample1'+'.genomes'
-    for root, dirs, filenames in os.walk(rootdir):
-    	for f in filenames:    
-	     name=re.sub(r'\.fasta$', '', f)
-             shell("echo {f} >> Sample1.pilon/Sample1.output.log;java -jar pilon --genome {rootdir}/{f} --frags Sample1.pilon/Sample1.sorted.bam --output Sample1.pilon/{name} --fix bases --tracks --vcf --changes  1>> Sample1.pilon/Sample1.output.log 2>&1")
+    input:'{sample}.genomes'
+    output:'{sample}.pilon/{sample}.output.log'
+    run:
+        rootdir = os.getcwd()+'/'+'Sample1'+'.genomes'
+    	for root, dirs, filenames in os.walk(rootdir):
+            for f in filenames:
+                name=re.sub(r'\.fasta$', '', f)
+                shell("echo {f} >> Sample1.pilon/Sample1.output.log;java -jar pilon --genome {rootdir}/{f} --frags Sample1.pilon/Sample1.sorted.bam --output Sample1.pilon/{name} --fix bases --tracks --vcf --changes  1>> Sample1.pilon/Sample1.output.log 2>&1")
