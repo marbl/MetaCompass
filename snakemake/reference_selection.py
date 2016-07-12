7b75883680afca5cbf6e7a4825c56b15714a8897
vcepeda <vickycees@gmail.com>
2016-07-12 11:24:12 -0400

    updates

--- snakemake/reference_selection.py
+++ snakemake/reference_selection.py
@@ -9,9 +9,15 @@ rule kmer_mask:
     threads:config["nthreads"]
     shell : "kmer-mask -ms 28 -mdb ./MetaCompass/refseq/kmer-mask_db/markers.mdb -1 {input} -clean 0.0 -match 0.01 -nomasking -t {threads} -l 103 -o {params}"
 
+rule fastq2fasta:
+    input: rules.kmer_mask.output
+    output:'{sample}.fasta'
+    message: """---Converting fastq to fasta."""
+    shell : "perl ./MetaCompass/bin/fq2fa.pl -i {input} -o {output}"
+
 rule reference_recruitment:
     input:
-        rules.kmer_mask.output
+        rules.fastq2fasta.output
     output:
         out = '{sample}.assembly.out',
 	reffile = '{sample}.assembly.out/mc.refseq.fna'
