5a1114fad3944634cc6fe1d61208a394ce85d129
vcepeda <vickycees@gmail.com>
2016-07-12 15:44:23 -0400

    new bowtie2 parameters

--- snakemake/mapping.py
+++ snakemake/mapping.py
@@ -10,4 +10,4 @@ rule bowtie2_map:
     log: '{sample}.step2.log'
     threads:config["nthreads"]
     message: """---Build index ."""
-    shell:"bowtie2-build --threads {threads} -q {input.ref} {output.pref} 1>> {output.index} 2>&1;bowtie2 -L 31 -p {threads} -x {output.pref} -f {input.reads} -S {output.sam} > {log} 2>&1"
+    shell:"bowtie2-build --threads {threads} -q {input.ref} {output.pref} 1>> {output.index} 2>&1;bowtie2 --end-to-end -D 15 -R 2 -N 0 -L 31 -i S,1,1.15 -p {threads} -x {output.pref} -f {input.reads} -S {output.sam} > {log} 2>&1"
