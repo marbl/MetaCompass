95f89479ef4e3d7bee21611793daaada57c38097
vcepeda <vickycees@gmail.com>
2016-08-01 10:59:45 -0400

    changed input fasta to fastq file and add -a parameter for bowtie2

---
+++ snakemake/mapping.py
@@ -0,0 +1,15 @@
+configfile: "config.json"
+rule bowtie2_map:
+    input:
+       ref='{sample}.assembly.out/mc.refseq.fna',
+       #reads='{sample}.fasta'
+       reads='{sample}.fastq'
+    output:
+       index= '{sample}.assembly.out/{sample}.index',
+       pref='{sample}.assembly.out/{sample}.index',
+       sam= '{sample}.assembly.out/{sample}.sam'
+    log: '{sample}.step2.log'
+    threads:config["nthreads"]
+    message: """---Build index ."""
+    #shell:"bowtie2-build --threads {threads} -q {input.ref} {output.pref} 1>> {output.index} 2>&1;bowtie2 --end-to-end -D 15 -R 2 -N 0 -L 31 -i S,1,1.15 -p {threads} -x {output.pref} -f {input.reads} -S {output.sam} > {log} 2>&1"
+    shell:"bowtie2-build --threads {threads} -q {input.ref} {output.pref} 1>> {output.index} 2>&1;bowtie2 -a --end-to-end -D 15 -R 2 -N 0 -L 31 -i S,1,1.15 -p {threads} -x {output.pref} -q {input.reads} -S {output.sam} > {log} 2>&1"
