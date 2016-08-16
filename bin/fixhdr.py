dee95bd302d006429c903cd6cc00a915aca7d8e7
Todd Treangen <treangen@gmail.com>
2016-08-16 09:22:04 -0400

    fix fixhdr, update pilon params, add tutorial example

--- bin/fixhdr.py
+++ bin/fixhdr.py
@@ -2,10 +2,20 @@ import os,sys,string
 
 f1 = open(sys.argv[1],'r')
 outf = open(sys.argv[1]+".fna",'w')
-hdr = f1.readline()
-fasta = f1.read()
-hdr = hdr.rsplit("_",1)[0]
 
-outf.write(hdr+"\n")
-outf.write(fasta)
+ctgs = f1.read().split(">")[1:]
+for ctg in ctgs:
+    hdr,fasta = ctg.split("\n",1)
+    hdr = hdr.rsplit("_",1)[0]
+    outf.write(">"+hdr+"\n")
+    width = 80
+    i = 0
+    while i+width < len(fasta):
+        outf.write(fasta[i:i+width]+"\n")
+        i+=width
+    if len(fasta[i:]) > 0:
+        outf.write(fasta[i:]+"\n")
+ 
+
 f1.close()
+outf.close()
