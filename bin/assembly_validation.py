c053497827d762d6ccf983607ff49b00d1a9fb35
mathieualmeida <mathieu.almeida77@gmail.com>
2016-11-20 21:16:26 -0500

    Shared contigs assignment added. Gene and marker gene detection added

--- bin/assembly_validation.py
+++ bin/assembly_validation.py
@@ -78,12 +78,16 @@ def compare_contigs_to_ref(contig_file, ref_folder, list_ref, result_dir):
       genome_name = ".".join(ref.split('.')[:-1])
       print (genome_name)
       if not os.path.isfile("%s/%s.done" % (result_dir, genome_name)): 
-         #nucmer comparison
-         cmd = "tsub 'nucmer -l 100 {0} {1}/{2}.mc.fasta -p {1}/{2} &>{1}/{2}.log; "\
+         #nucmer and gene prediction comparison
+         cmd = "nucmer -l 100 {0} {1}/{2}.mc.fasta -p {1}/{2} &>{1}/{2}.log; "\
             "delta-filter -i 95 -l 100 {1}/{2}.delta > {1}/{2}.delta.filt; "\
             "dnadiff -d {1}/{2}.delta.filt -p {1}/{2} &>>{1}/{2}.log 2>&1; "\
-            "touch {1}/{2}.done' -q throughput -l nodes=1:ppn=1,mem=10gb"\
-            ",walltime=00:30:00".format(complete_ref,result_dir,genome_name)
+            "prodigal -i {1}/{2}.mc.fasta -d {1}/{2}.mc.fna -a {1}/{2}.mc.faa -q > /dev/null; "\
+            "sleep 1s; "\
+            "fetchMG.pl -m extraction {1}/{2}.mc.faa -o {1}/{2}.fetchMG > /dev/null; "\
+            "cat {1}/{2}.fetchMG/*.fna > {1}/{2}.mc.marker.fna; "\
+            "touch {1}/{2}.done".format(complete_ref,result_dir,genome_name)
+         
          os.system(cmd)
 
 
@@ -91,7 +95,6 @@ def create_report_file(list_ref, result_dir, output_name):
 
    #wait till all report files are generated
    nb_genome = len(list_ref)
-
    process_cmd = subprocess.Popen('ls {0}/*.done | wc -l'.format(result_dir),
                                   shell=True, stdout=subprocess.PIPE)
    
@@ -105,10 +108,11 @@ def create_report_file(list_ref, result_dir, output_name):
       nb_done = int(process_cmd.communicate()[0])
 
    with open (output_name, 'wt') as output_file:
-      output_file.write("%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\n" % ("genome_name", 
+      output_file.write("%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\n" % ("genome_name", 
                         "nb_contig", "total_ref_bases", "aligned_bases", "%_ref_recovered", 
                         "%_avg_identity", "avg_alignment_len", "total_snps", 
-                        "total_gsnps"))
+                        "total_gsnps","total_breakpoints","total_gene","total_complete_gene",
+                        "total_marker_gene","total_complete_marker_gene"))
       
       for ref in list_ref:
          genome_name = ".".join(ref.split('.')[:-1])
@@ -121,7 +125,47 @@ def create_report_file(list_ref, result_dir, output_name):
          avg_alignment_len = "NA"
          total_snps= "NA"
          total_gsnps = "NA"
- 
+         breakpoints = "NA"
+         nb_gene = 0
+         nb_complete_gene = 0
+         nb_marker_gene = 0
+         nb_complete_marker_gene = 0
+
+         #nb_gene
+         process_cmd = subprocess.Popen('grep "^>" {0}/{1}.mc.fna -c'.format(result_dir,genome_name),
+                                  shell=True, stdout=subprocess.PIPE)
+         try:
+            nb_gene = int(process_cmd.communicate()[0])
+            print(nb_gene)
+         except:
+            nb_gene = 0
+         #nb_complete_gene
+         process_cmd = subprocess.Popen('grep "partial=00" {0}/{1}.mc.fna -c'.format(result_dir,genome_name),
+                                  shell=True, stdout=subprocess.PIPE)
+         try:
+             nb_complete_gene = int(process_cmd.communicate()[0])
+             print(nb_complete_gene)
+         except:
+             nb_complete_gene = 0
+
+         #nb_marker_gene
+         process_cmd = subprocess.Popen('grep "^>" {0}/{1}.mc.marker.fna -c'.format(result_dir,genome_name),
+                                  shell=True, stdout=subprocess.PIPE)
+         try:
+       	     nb_marker_gene = int(process_cmd.communicate()[0])
+             print(nb_marker_gene)
+         except:
+             nb_marker_gene = 0
+
+         #nb_complete_marker_gene
+         process_cmd = subprocess.Popen('grep "partial=00" {0}/{1}.mc.marker.fna -c'.format(result_dir,genome_name),
+                                  shell=True, stdout=subprocess.PIPE)
+         try:
+             nb_complete_marker_gene = int(process_cmd.communicate()[0])
+             print(nb_complete_marker_gene)
+         except:
+             nb_complete_marker_gene = 0 
+         
          with open(report_file_name,'rt') as report_file:
             for line in report_file:
                if "AlignedSeqs" in line:
@@ -138,15 +182,19 @@ def create_report_file(list_ref, result_dir, output_name):
                   total_snps = line.split()[2].strip()
                elif "TotalGSNPs" in line:
                   total_gsnps = line.split()[2].strip()
+               elif "Breakpoints" in line:
+                  breakpoints = line.split()[2].strip()
+
             perc_recovered_ref = (float(aligned_bases) / float(total_ref_bases)) * 100
-            output_file.write("%s\t%s\t%s\t%s\t%.4f\t%s\t%s\t%s\t%s\n" % (
+            output_file.write("%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\n" % (
                               genome_name, nb_contig, total_ref_bases, aligned_bases, 
                               perc_recovered_ref, avg_identity,
-                              avg_alignment_len, total_snps, total_gsnps))
+                              avg_alignment_len, total_snps, total_gsnps, breakpoints,
+                              nb_gene,nb_complete_gene,nb_marker_gene,nb_complete_marker_gene))
 
    os.system("rm {0}/*.delta {0}/*.1delta {0}/*.qdiff {0}/*.rdiff {0}/*.snps"\
              " {0}/*.unqry {0}/*.mcoords {0}/*.mdelta {0}/*.delta.filt"\
-             " {0}/*.1coords ".format(result_dir))
+             " {0}/*.1coords".format(result_dir))
 
 
 def create_contig_files(contig_file, ref_folder, list_ref, result_dir):
@@ -164,29 +212,114 @@ def create_contig_files(contig_file, ref_folder, list_ref, result_dir):
 
    print('align sequences...')
    os.system("makeblastdb -in {0}/reference.fasta -out {0}/reference -dbtype nucl".format(result_dir))
-   os.system("blastn -db {0}/reference -query {1} -word_size 100 -evalue 1e-3"\
-                " -max_target_seqs 1 -perc_identity 95 -outfmt 6 -out {0}/contig_to_ref.blastn".format(
+   os.system("blastn -db {0}/reference -query {1} -word_size 40 -evalue 1e-3"\
+                " -max_target_seqs 100 -perc_identity 95 -outfmt 6 -out {0}/contig_to_ref.blastn".format(
                 result_dir, contig_file))
 
-   print('extract contig reference best-hit')
+   print('extract contig reference best-hit only')
    with open(result_dir + '/contig_to_ref.blastn', 'rt') as blast_file:
       for line in blast_file:
-         items=line.split()
-         contig_name = items[0].strip()
-         ref_name = items[1].strip()
-         if not contig_to_ref.get(contig_name):
-            contig_to_ref[contig_name] = ref_name
-
+   	  items=line.split()
+   	  contig_name = items[0].strip()
+   	  ref_name = items[1].strip()
+   	  if not contig_to_ref.get(contig_name):
+             contig_to_ref[contig_name] = ref_name
    print('create fasta file for each ref')
+   
    for genome in list_ref:
       genome_name = genome.replace(".fasta","")
       for contig in contig_to_ref:
-         if contig_to_ref[contig] == genome_name:
+   	  if contig_to_ref[contig] == genome_name:
             cmd = 'grep "^>{0}" -A 1 -m 1 {1}/mc.oneline.fasta >> {1}/{2}.mc.fasta'.format(
                contig, result_dir, genome_name)
-            os.system(cmd)                 
+            os.system(cmd)
+
+
+#=========
+#
+#=========
+def create_contig_files_shared(contig_file, ref_folder, list_ref, result_dir,contig_list):
+   genome_contig_list = {}
+
+   #create ref db that contain all reference
+   for genome in list_ref:
+      genome_name = genome.replace(".fasta","")
+      os.system("sed 's/>/>{0} /g' {1}/{0}.fasta >> {2}/reference.fasta".format(
+                                           genome_name,ref_folder, result_dir))
+
+   print('align sequences...')
+   os.system("makeblastdb -in {0}/reference.fasta -out {0}/reference -dbtype nucl > /dev/null".format(result_dir))
+   os.system("blastn -db {0}/reference -query {1} -word_size 100 -evalue 1e-2"\
+                " -max_target_seqs 100 -perc_identity 95 -outfmt 6 -out {0}/contig_to_ref.blastn".format(
+                result_dir, contig_file))
+
+   print('extract contig reference all bests-hits')
+   with open(result_dir + '/contig_to_ref.blastn', 'rt') as infile:
+      line = infile.readline()
+      items = line.split('\t')
+      oldquery = items[0]
+      oldscore = items[-1].strip()
+      infile.seek(0)
+      
+      for line in infile:
+         items=line.split('\t')
+         query=items[0]
+         score=items[-1].strip()
+         extract=False
+         update=True
+
+         if oldquery == query:
+            if oldscore == score:
+               extract=True
+            else:
+               update=False
+         else:
+            extract=True
+
+         if extract and line:
+            contig_name = items[0].strip()
+            ref_name = items[1].strip()
+            if not genome_contig_list.get(ref_name):
+               genome_contig_list[ref_name] = []
+
+            if not contig_name in genome_contig_list[ref_name]:
+               genome_contig_list[ref_name].append(contig_name)
+
+         #keep the best score for same contig
+         if update:
+            oldquery=query
+            oldscore=score
+
+   print('create fasta file for each ref')
+   for genome in genome_contig_list:
+       filename = '{0}/{1}.mc.fasta'.format(result_dir,genome)
+
+       with open(filename, 'wt') as genome_file:
+          for contig in genome_contig_list[genome]:
+             genome_file.write(">{0}\n{1}\n".format(contig,contig_list[contig]))
+
       
       
+def extract_contig_list(contigfilename):
+   contig_list = {}
+   with open(contigfilename, 'rt') as contigfile:
+      header=''
+      seq=''
+      for line in contigfile:
+         if line[0]=='>':
+            if header!='':
+               contig_list[header] = seq
+            header=line.strip().split()[0][1:]
+            seq=''
+         else:
+            seq += line.strip()
+
+      #last sequence
+      contig_list[header] = seq
+
+   return contig_list
+
+
 def main():
 
    """Main program function
@@ -200,8 +333,11 @@ def main():
 
    list_ref = extract_list_ref(args.ref_folder)
 
+   #extract contig sequence
+   contig_list = extract_contig_list(args.contig_file)
+
    #create metaG contig files
-   create_contig_files(args.contig_file, args.ref_folder, list_ref, args.result_dir)
+   create_contig_files_shared(args.contig_file, args.ref_folder, list_ref, args.result_dir, contig_list)
 
    #compare each contigs to ref
    compare_contigs_to_ref(args.contig_file, args.ref_folder, list_ref, args.result_dir)      
