8c3c55c78d1da955ae3ab63495270362fc67e395
mathieualmeida <mathieu.almeida77@gmail.com>
2017-02-22 16:11:21 -0500

    best strata now based on XS and AS scores

--- bin/best_strata.py
+++ bin/best_strata.py
@@ -8,48 +8,59 @@ output_name=sys.argv[2]
 with open (input_name, 'rt') as infile,\
    open (output_name,'wt') as outfile:
 
+   #skip header
    line = infile.readline()
-
    while line[0]=="@":
       last_pos = infile.tell()
       line = infile.readline()
 
-   items = line.split('\t')
-   oldquery = items[0]
-
-   for score in items[12:]:
-      if score[:2]=="NM":
-         oldNM = score.split(':')[2]
-         break
    infile.seek(last_pos)
 
-   #print('start parsing...')
+   #extract best-hits
    for line in infile:
-      items=line.split('\t')
-      query=items[0]
 
-      for score in items[12:]:
-         if score[:2]=="NM":
-            NM = score.split(':')[2]
-            break
+      items = line.split('\t')
 
-      extract=False
-      update=True
-
-      if oldquery == query:  
-         if oldNM == NM: 
-            extract=True
-         else:
-            update=False
-      else:
-         extract=True
-   
-      if extract and line:
+      if "XS" in items[12]: #multiple mapping --> test best-strata
+         XS=int(items[12].split(':')[2],10)
+         AS=int(items[11].split(':')[2],10)
+         if AS>=XS:
+            outfile.write(line)
+      else: #unique mapping --> save
          outfile.write(line)
+      
+#print('end')
 
-      #keep the best score for same read
-      if update:
-         oldquery=query
-         oldNM=NM
 
-   #print('end')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
