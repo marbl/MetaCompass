ac42aa1f47c3f16dfcc4464f8019d6e0ea2b6790
victoria paz cepeda <vcepeda@umiacs.umd.edu>
2019-04-05 18:48:56 -0400

    Selectiing limited refseq assemblies pertax

--- bin/select_references.py
+++ bin/select_references.py
@@ -3,14 +3,14 @@
 #############################################
 #
 # Program: Pick reference genomes based on
-#          metaphyler output
+#          marker genes 
 #
 # Author: Victoria Cepeda
 #
-# Sat Dec 22 12:55:00 EDT 2018
+# Fri Apr 05 18:00:00 EDT 2019
 #
-# Description:uses "/src/markers/markers.length",
-# "/src/markers/markers.numcogs", and blastn  
+# Description:uses "/refseq/markers/markers.length",
+# "/refseq/markers/markers.numcogs", and blastn  
 # output to calculate depth of coverage and 
 # output the fasta file of candidate references
 #############################################
@@ -36,7 +36,7 @@ def process_marker_db(marker2alignlen):
    marker2length={}
    marker2numcog={}
    mlength=0
-   with open(pathbin + "/src/markers/markers.length", "rt") as markerfile:
+   with open(pathbin + "/refseq/markers/markers.length", "rt") as markerfile:
    
       for line in markerfile:
          items = line.split("\t")
@@ -49,13 +49,12 @@ def process_marker_db(marker2alignlen):
                marker2length[marker] += mlength
             else:
                marker2length[marker] = mlength
-   with open(pathbin + "/src/markers/markers.numcogs", "rt") as markerfile:
+   with open(pathbin + "/refseq/markers/markers.numcogs", "rt") as markerfile:
       for line in markerfile:
           items = line.split("\t")
           marker=items[0]
           numcogs=int(items[1])
           marker2numcog[marker] = numcogs
-   #print ("%s" % marker2numcog)
 
    return marker2length,marker2numcog
 
@@ -84,7 +83,7 @@ def extract_blast_hits(blast_filename):
             markertotalcount[marker] += 1
          else:
             markertotalcount[marker] = 1
-   #print ("extract_blast_hits:marker2alignlen,markertotalcount %s  %s" % (marker2alignlen,markertotalcount))
+   print ("extract_blast_hits:marker2alignlen,markertotalcount %s  %s" % (marker2alignlen,markertotalcount))
    return marker2alignlen,markertotalcount
 
 #============================================================
@@ -96,20 +95,19 @@ def extract_blast_hits(blast_filename):
 #   refid_filename = outdir+"/"+prefix+".refseq.ids"#sys.argv[1]
 #   assembly_id_filename = outdir+"/"+prefix+".assembly.ids"#sys.argv[1]
 #   markercoverage_filename = outdir+"/"+prefix+ ".markercoverage.txt"#sys.argv[1]
-
 #return total_marker_cov,myset
 #total_marker_cov has dic of marker and summation marker2alignlen[marker]/marker2lenght[marker])
 #============================================================
-#refid_filename, 
 def marker_cov(marker2alignlen, marker2lenght,covthreshold):
    marker_cov={}
    #calculate coverage per marker gene
    #only output markers with minimum selected coverage=covthreshold of 1
    for marker in marker2alignlen:#is ref not marker
       coverage= float(marker2alignlen[marker]/marker2lenght[marker])  
+      print ("marker, cov: %s %f" % (marker,coverage))
       if coverage >= covthreshold:
          marker_cov[marker] = coverage  
-   #print ("marker_cov: %s" % marker_cov)
+   print ("marker_cov: %s" % marker_cov)
    return marker_cov
 #refid_filename = outdir+"/"+prefix+".refseq.ids"#sys.argv[1]
 #assembly_id_filename = outdir+"/"+prefix+".assembly.ids"#sys.argv[1]
@@ -117,8 +115,6 @@ def marker_cov(marker2alignlen, marker2lenght,covthreshold):
 #cog_filename = outdir+"/"+prefix+ ".cogmarkercoverage.txt"#sys.argv[1]   
 #marker2numcog
 
-#with open('a', 'w') as a, open('b', 'w') as b:
-#    do_something()
 def create_aux_dics2(markertotalcount, covthreshold,marker_cov):   
    #from highest to lowest coverage print marker and totalcoverage per marker
    #in addition, add refid(NC_) to ids list; add assemblyid(GCF) to ass list 
@@ -145,7 +141,7 @@ def create_aux_dics2(markertotalcount, covthreshold,marker_cov):
                acc_ref[assembly_acc].append(ref_acc)
        else:
             acc_ref[assembly_acc]=[ref_acc]
-   
+   print ("%s" % assembly_acc)
    return cumulative_cov,cog_num,acc_ref
 
 
@@ -156,11 +152,16 @@ def cog_stats2(markertotalcount,refid_filename, assembly_id_filename, assembly_m
    #print marker and coverae
    outfile = open(cog_filename,'w')
    for acc, cog in cog_list.items():
-       if len(cog) >= 10 and cumulative_cov[acc]>=50:#at least 10 cogs with deep of coverage 1
+       print ( "acc,cog, len(cog), cumulative_cov[acc]: %s\t%s\t%d\t%d" % (acc, cog,len(cog),cumulative_cov[acc]), file= outfile)       
+#       if len(cog) >= 10 and cumulative_cov[acc]>=50:#at least 10 cogs with deep of coverage 1
+       
+       if len(cog) >= 1 and cumulative_cov[acc]>=len(cog):#at least 10 cogs with deep of coverage 1
            acc_list.append(acc)
-           print ( "%s\t%s\t%d" % (acc, cog,len(cog)), file= outfile)       
+           print ( "acc, cog,len(cog)):%s\t%s\t%d" % (acc, cog,len(cog)))       
    outfile.close()
    print (cog_list)
+   print (acc_list)
+   
 
    outfile = open(assembly_markercoverage_filename,'w')         
    for acc, cov in sorted(cumulative_cov.items(), key=operator.itemgetter(1), reverse=True): 
@@ -176,7 +177,6 @@ def cog_stats2(markertotalcount,refid_filename, assembly_id_filename, assembly_m
    #   assembly_id_filename = outdir+"/"+prefix+".assembly.ids"#sys.argv[1]
    outfile = open(assembly_id_filename,'w')
    #   ###arreglar
-   print ( "##########ACC_LIST%s" % (acc_list))
    for acc in acc_list:
        print ("%s" % (acc), file =outfile)
    outfile.close()
@@ -185,12 +185,8 @@ def cog_stats2(markertotalcount,refid_filename, assembly_id_filename, assembly_m
 def calculate_mash(ref2hash,cutoff):    
     refids=[]
     if len(ref2hash)==1:
-        print ("GFCUNIQ len(ref2hash)==1" )
-        print ("ref2hash.keys:%s" % ref2hash.keys())
-        print ("ref2hash.values:%s" % ref2hash.values())
         for ref, hashn in ref2hash.items():
             return [ref]
-        #return [ref2hash.keys()]
     max_value=max(ref2hash.values())
     min_value=min(ref2hash.values())
     avg_value=sum(ref2hash.values())/len(ref2hash)
@@ -209,13 +205,28 @@ def calculate_mash(ref2hash,cutoff):
             max_value=hashes
             refids.append(refid)
     return refids        
-        
+def highest_mash(ref2hash):    
+    refids=[]
+    if len(ref2hash)==1:
+        print ("GFCUNIQ len(ref2hash)==1" )
+        print ("ref2hash.keys:%s" % ref2hash.keys())
+        print ("ref2hash.values:%s" % ref2hash.values())
+        for ref, hashn in ref2hash.items():
+            return [ref]
+    max_value=max(ref2hash.values())
+    min_value=min(ref2hash.values())
+    avg_value=sum(ref2hash.values())/len(ref2hash)
 
-#elegir tax con menos de 10% de variacion en shared hashes. si todos son menos de 10%similares, elegir al azar uno de ellos. leer que hace kraken.
-#The fields are [identity, shared-hashes, median-multiplicity, p-value, query-ID, query-comment]:
+    #return first highest mash
+    for refid, hashes in sorted(ref2hash.items(), key=operator.itemgetter(1),reverse=True):
+            if hashes == max_value:
+                return [refid]
+
+#select assembly tax <x%[x<=10] of variation in # of shared-hashes. Ties are resolved randomly.
+#mash screen fields are [identity, shared-hashes, median-multiplicity, p-value, query-ID, query-comment]:
 def parse_mash_screen(screen_bacgeno,tax_dic,tax_dic_old,filt_assembly_id_filename,filt_assembly_fna_filename,filt_tax_file,cutoff):
-   ref2tax={}
-   ref2hash={}
+   ref2tax={} #filtered results, 1 per tax
+   ref2hash={}#mash screen results
    with open(screen_bacgeno, "rt") as mashfile:
         for line in mashfile:
          items = line.split("\t")
@@ -228,16 +239,18 @@ def parse_mash_screen(screen_bacgeno,tax_dic,tax_dic_old,filt_assembly_id_filena
          ref2hash[queryid]=hashes
    temp2hash={}
    for tax,refs in tax_dic_old.items():
-       for refid in refs:#refs is list of GCFIDS assigned to tax
-           if refid not in temp2hash:
-               temp2hash[refid]=ref2hash[refid]
-           else:
-               temp2hash[refid].append(ref2hash[refid])
+       for refid in refs:#refs is list of assembly IDS assigned to same tax
+           if refid in ref2hash:#mash screen can have zero matches and need to be chacked here
+               if refid not in temp2hash:
+                   temp2hash[refid]=ref2hash[refid]
+               else:
+                   temp2hash[refid].append(ref2hash[refid])
+       if temp2hash:
+           ref2tax[tax]=highest_mash(temp2hash)
+           #ref2tax[tax]=calculate_mash(ref2hash,cutoff)#option to keep some hashes per tax    
+           temp2hash.clear()       
 
-       ref2tax[tax]=calculate_mash(temp2hash,cutoff)  
-       temp2hash.clear()       
-
-   #write filt_assembly_id_filename,filt_tax_file
+   #write filt_tax_file,filt_assembly_id_filename
    outfile = open(filt_tax_file,'w')
    for tax, ref in ref2tax.items():
        print ( "%s\t%s" % (tax, ref), file =outfile)
@@ -249,17 +262,14 @@ def parse_mash_screen(screen_bacgeno,tax_dic,tax_dic_old,filt_assembly_id_filena
            print ("%s" % (i), file =outfile)
    outfile.close()
    
-### 3 cogs and 10 total coverage, and that has to cover some fraction of genome.
-#===========================
-#MAIN
-#===========================
+
 def main():
 #----------------------------------------#
 # read command line options
 #----------------------------------------#
    query = ""
    blast = "blastn"
-   outdir=""
+   outdir = ""
    prefix = "mc"
    nump = 0
    query  = sys.argv[1]
@@ -268,16 +278,19 @@ def main():
    nump   = sys.argv[4]
    covthreshold = float(sys.argv[5])
 #----------------------------------------#
-   ref = pathbin +"/src/markers/markers.refseq.dna"
+   ref = pathbin +"/refseq/markers/markers.refseq.dna"
+   
    param = "-word_size 28"
 # run blast
-   cmd = "%s %s -num_threads %s -evalue 1e-10 -perc_identity 97 -outfmt 6 -max_target_seqs 100 -query %s -db %s > %s/%s.%s.all"%(blast,param,nump,query,ref,outdir,prefix,blast)
+   #cmd = "%s %s -num_threads %s -evalue 1e-10 -perc_identity 97 -outfmt 6 -max_target_seqs 100 -query %s -db %s > %s/%s.%s.all"%(blast,param,nump,query,ref,outdir,prefix,blast)
+   cmd = "%s %s -num_threads %s -evalue 1e-20 -perc_identity 97 -outfmt 6 -query %s -db %s > %s/%s.%s.all"%(blast,param,nump,query,ref,outdir,prefix,blast)
+
    print ("%s" % (cmd))
    os.system(cmd)
 # filter blast best hit
-   cmd = "%s/bin/best-hits.py %s/%s.%s.all %s/%s.%s"%(pathbin,outdir,prefix,blast,outdir,prefix,blast)
-   print ("%s" % (cmd))
-   os.system(cmd)
+   #cmd = "%s/bin/best-hits.py %s/%s.%s.all %s/%s.%s"%(pathbin,outdir,prefix,blast,outdir,prefix,blast)
+   #print ("%s" % (cmd))
+   #os.system(cmd)
     
 #if (scalar @ARGV == 3) {
 #{input} {output.out} {threads} {params.mincov} {params.readlen}
@@ -311,14 +324,14 @@ def main():
    cog_stats2(markertotalcount,refid_filename, assembly_id_filename, assembly_markercoverage_filename, cog_filename,marker2numcog,cumulative_cov,cog_num,acc_ref)
 
    print ("# Extract reference genome sequences")
-   #buscar ids del mismo assembly project
+   #search ids from same assembly project
    cmd ="for file in $(cat %s/%s.assembly.ids);do cat  %s/refseq/genomes/${file}/${file}.fasta >> %s/%s.refseq.fna;done"%(outdir,prefix,pathbin,outdir,prefix)
    print ("%s" % (cmd))
    os.system(cmd)
    cmd ="grep '>'  %s/%s.refseq.fna|tr -d '>'|cut -f1 -d ' '> %s/%s.refseq.ids" %(outdir,prefix,outdir,prefix)
    print ("%s" % (cmd))
    os.system(cmd)
-   refseq_tax = pathbin + "/src/markers/refseq_tax"
+   refseq_tax = pathbin + "/refseq/markers/refseq_tax"
    
    tax_file = outdir + "/"+ prefix + ".tax"
    cmd="grep -f %s/%s.assembly.ids %s |sort -k2,2 >>%s/%s.tax" %(outdir,prefix,refseq_tax,outdir,prefix)
@@ -333,7 +346,6 @@ def main():
    with open(tax_file, "rt") as taxfile:
        for line in taxfile:
            items = line.split("\t")
-           #print ("%s" % items)
            ref_acc=items[0]
            tax=int(items[1])
            if tax in tax_dic_old:
@@ -350,18 +362,13 @@ def main():
    with open(tax_file, "rt") as taxfile:
        for line in taxfile:
            items = line.split("\t")
-           #print ("%s" % items)
            ref_acc=items[0]
            tax=int(items[1])
            if ref_acc in tax_dic:
                tax_dic[ref_acc].append(tax)
            else:
-               tax_dic[ref_acc] = [tax]#elegir taxs con menos de 5% de variacion. si todos son menos de 5%similares, elegir al azar uno de ellos. leer que hace kraken.
-   #parse tax file and put same id seqs in a dictionary
-   #run mash sketch
-   #cmd="%s/bin/mash-Linux64-v2.1/mash sketch -p %s -k 21 -s 1000 -i  %s/%s.refseq.fna"%(pathbin,nump,outdir,prefix)
-   #print ("%s" % (cmd))
-   #os.system(cmd)
+               tax_dic[ref_acc] = [tax]
+
    cmd ="for file in $(cat %s/%s.assembly.ids);do echo  %s/refseq/genomes/${file}/${file}.fasta.msh >> %s/%s.msh.list;done"%(outdir,prefix,pathbin,outdir,prefix)
    print ("%s" % (cmd))
    os.system(cmd)  
@@ -374,10 +381,9 @@ def main():
    cmd="%s/bin/mash-Linux64-v2.1/mash screen -p %s %s/%s.assembly.msh  %s >%s/screen_bacgeno.tab"%(pathbin,nump,outdir,prefix,reads,outdir)
    print ("%s" % (cmd))
    os.system(cmd)
-   #parse mash screen, sort from highest to lowest # share hashes
-   filt_assembly_id_filename = outdir+"/"+prefix+".filt.assembly.ids"#sys.argv[1]
-   #filt_assembly_fna_filename = outdir+"/"+prefix+".filt.assembly.fna"#sys.argv[1]
-   filt_assembly_fna_filename = outdir+"/"+prefix+".refseq.filt.fna"#sys.argv[1]
+   #parse mash screen, sort from highest to lowest # shared-hashes
+   filt_assembly_id_filename = outdir+"/"+prefix+".filt.assembly.ids"
+   filt_assembly_fna_filename = outdir+"/"+prefix+".refseq.filt.fna"
    
    filt_tax_file=outdir + "/"+ prefix + ".filt.tax"
    cutoff=1.1
@@ -385,11 +391,20 @@ def main():
    cmd ="for file in $(cat %s);do cat %s/refseq/genomes/${file}/${file}.fasta >> %s;done"%(filt_assembly_id_filename,pathbin,filt_assembly_fna_filename)
    print ("%s" % (cmd))
    os.system(cmd)
+   ###new addition to map acc to assemblies
+   cmd ="for file in $(cat %s);do acc=$(cut -f1 %s/refseq/genomes/${file}/${file}.fasta.fai); echo $file'\t'$acc >> %s;done"%(filt_assembly_id_filename,pathbin,filt_assembly_id_filename+".map")
+   print ("%s" % (cmd))
+   os.system(cmd)
+   
+   cmd ="for file in $(cat %s);do acc=$(cut -f1 %s/refseq/genomes/${file}/${file}.fasta.fai); for j in $acc;do echo $file'\t'$j >> %s;done;done"%(filt_assembly_id_filename,pathbin,filt_assembly_id_filename+".map2acc")
+   print ("%s" % (cmd))
+   os.system(cmd)
+   
+   
    cmd ="grep '>'  %s/%s.refseq.filt.fna|tr -d '>'|cut -f1 -d ' '> %s/%s.refseq.filt.ids" %(outdir,prefix,outdir,prefix)
    print ("%s" % (cmd))
    os.system(cmd)
-   #elegir taxs con menos de 5%-10% de variacion. si todos son menos de 5%similares, elegir al azar uno de ellos. leer que hace kraken.
-
+   
 if __name__ == '__main__':
     main()
  
