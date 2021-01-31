2cdeb979ccb5264f0dbac868b4c884687424e3f5
Victoria Cepeda <vickycees@gmail.com>
2021-01-31 18:58:48 -0500

    Update metacompass.unpaired.py

--- snakemake/metacompass.unpaired.py
+++ snakemake/metacompass.unpaired.py
@@ -141,7 +141,7 @@ rule polish_map:
     params:
         retry='%s/error_correction/.run1.ok'%(config['outdir'])
     threads:int(config["nthreads"])
-    message: """---Map reads for polish polishing."""
+    message: """---Map reads for polishing."""
     shell:"bowtie2-build --threads {threads} -q {input.ref} {output.pref} 1>> {output.index} 2>&1;bowtie2 --no-mixed --sensitive --no-unal -p {threads} -x {output.pref} -q -U {input.ru} -S {output.sam} --un {output.unmappedru} >> {output.log} 2>&1 && touch {params.retry}"
 
 rule sam_to_bam:
